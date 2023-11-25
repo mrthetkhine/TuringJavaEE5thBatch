6474dfacfc0ab5b9844a27cb0e18a9597c752306
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2023-11-25 20:25:42 +0630

    add db dump

---
+++ spring-boot-jpa-demo/db-dump/db.sql
@@ -0,0 +1,165 @@
+CREATE DATABASE  IF NOT EXISTS `javaee5th_jpa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `javaee5th_jpa`;
+-- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
+--
+-- Host: localhost    Database: javaee5th_jpa
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `actor`
+--
+
+DROP TABLE IF EXISTS `actor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `actor` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(128) DEFAULT NULL,
+  `last_name` varchar(128) DEFAULT NULL,
+  `birthday` datetime DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actor`
+--
+
+LOCK TABLES `actor` WRITE;
+/*!40000 ALTER TABLE `actor` DISABLE KEYS */;
+INSERT INTO `actor` VALUES (1,'Leonardo','DiCaprio','1974-11-11 00:00:00','Male','2023-11-18 19:57:34',NULL),(2,'Kate','Winslet','1975-11-11 00:00:00','Female','2023-11-25 19:17:47','2023-11-25 19:53:12'),(3,'Samuel','Henry John Worthington','1976-11-11 00:00:00','Male','2023-11-25 19:19:01',NULL),(4,'Zoe','Yadira','1978-11-11 00:00:00',NULL,'2023-11-25 19:19:49',NULL),(5,'Susan','Alexandra',NULL,'Female','2023-11-25 19:47:30','2023-11-25 19:47:30');
+/*!40000 ALTER TABLE `actor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `actor_in_movie`
+--
+
+DROP TABLE IF EXISTS `actor_in_movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `actor_in_movie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `movie_id` int DEFAULT NULL,
+  `actor_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actor_in_movie`
+--
+
+LOCK TABLES `actor_in_movie` WRITE;
+/*!40000 ALTER TABLE `actor_in_movie` DISABLE KEYS */;
+INSERT INTO `actor_in_movie` VALUES (1,1,1),(2,1,2),(5,5,1),(6,2,3),(7,2,4),(8,2,5),(9,22,2);
+/*!40000 ALTER TABLE `actor_in_movie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `movie_id` int DEFAULT NULL,
+  `comment` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment`
+--
+
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+INSERT INTO `comment` VALUES (1,1,'Comment  1 for titantic',NULL,NULL),(2,1,'Comment 2 for titanic',NULL,NULL),(17,2,'COMMENT 1 FOR AVATAR','2023-11-25 18:54:35','2023-11-25 19:08:22'),(18,2,'COMENT 2 FOR AVATAR','2023-11-25 18:54:35','2023-11-25 19:08:22');
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie`
+--
+
+DROP TABLE IF EXISTS `movie`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `year` int DEFAULT NULL,
+  `genre` varchar(45) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie`
+--
+
+LOCK TABLES `movie` WRITE;
+/*!40000 ALTER TABLE `movie` DISABLE KEYS */;
+INSERT INTO `movie` VALUES (1,'Titanic',1997,'drama','2023-11-07 19:54:30',NULL),(2,'Avatar',2009,'Sci-Fi',NULL,NULL),(4,'Aliens',1986,'Sci-Fi',NULL,NULL),(5,'Inception',2010,'Sci-Fi',NULL,NULL),(6,'Tenet',2010,'Sci-Fi','2023-11-07 20:54:15',NULL),(7,'Jhon Wick 4',2023,'Action','2023-11-12 20:15:57',NULL),(8,'Jhon Wick 3',2019,'Action','2023-11-12 20:15:57',NULL),(9,'Jhon Wick 2',2017,'Action','2023-11-12 20:15:57',NULL),(20,'Test movie for comment',2023,'Drama','2023-11-25 18:48:47','2023-11-25 18:48:47'),(22,'Quill',2000,'Drama','2023-11-25 19:53:12','2023-11-25 19:53:12');
+/*!40000 ALTER TABLE `movie` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie_details`
+--
+
+DROP TABLE IF EXISTS `movie_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movie_details` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `movie_id` int DEFAULT NULL,
+  `details` varchar(45) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie_details`
+--
+
+LOCK TABLES `movie_details` WRITE;
+/*!40000 ALTER TABLE `movie_details` DISABLE KEYS */;
+INSERT INTO `movie_details` VALUES (1,1,'Titanic is Good movie',NULL,NULL),(2,2,'Avatar is good sci-fi movie',NULL,NULL),(3,4,'Aliens is good',NULL,NULL),(7,15,'Test movie 6 details updated',NULL,'2023-11-21 20:08:02');
+/*!40000 ALTER TABLE `movie_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-25 20:24:25
