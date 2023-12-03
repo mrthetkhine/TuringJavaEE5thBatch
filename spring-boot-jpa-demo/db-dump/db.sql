c7b60959c1afc2a47d4c35b011c7dcba918a3c59
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2023-12-03 20:21:03 +0630

    add db dump

--- spring-boot-jpa-demo/db-dump/db.sql
+++ spring-boot-jpa-demo/db-dump/db.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `javaee5th_jpa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `javaee5th_jpa`;
 -- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
 --
 -- Host: localhost    Database: javaee5th_jpa
@@ -72,6 +70,33 @@ INSERT INTO `actor_in_movie` VALUES (1,1,1),(2,1,2),(5,5,1),(6,2,3),(7,2,4),(8,2
 UNLOCK TABLES;
 
 --
+-- Table structure for table `bank_account`
+--
+
+DROP TABLE IF EXISTS `bank_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bank_account` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `account_name` varchar(255) DEFAULT NULL,
+  `balance` double DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bank_account`
+--
+
+LOCK TABLES `bank_account` WRITE;
+/*!40000 ALTER TABLE `bank_account` DISABLE KEYS */;
+INSERT INTO `bank_account` VALUES (1,'Aung Aung',0,NULL,'2023-12-03 19:32:35'),(2,'Maung Maung',200,NULL,'2023-12-03 19:32:35');
+/*!40000 ALTER TABLE `bank_account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `comment`
 --
 
@@ -113,7 +138,7 @@ CREATE TABLE `movie` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -122,7 +147,7 @@ CREATE TABLE `movie` (
 
 LOCK TABLES `movie` WRITE;
 /*!40000 ALTER TABLE `movie` DISABLE KEYS */;
-INSERT INTO `movie` VALUES (1,'Titanic',1997,'drama','2023-11-07 19:54:30',NULL),(2,'Avatar',2009,'Sci-Fi',NULL,NULL),(4,'Aliens',1986,'Sci-Fi',NULL,NULL),(5,'Inception',2010,'Sci-Fi',NULL,NULL),(6,'Tenet',2010,'Sci-Fi','2023-11-07 20:54:15',NULL),(7,'Jhon Wick 4',2023,'Action','2023-11-12 20:15:57',NULL),(8,'Jhon Wick 3',2019,'Action','2023-11-12 20:15:57',NULL),(9,'Jhon Wick 2',2017,'Action','2023-11-12 20:15:57',NULL),(20,'Test movie for comment',2023,'Drama','2023-11-25 18:48:47','2023-11-25 18:48:47'),(22,'Quill',2000,'Drama','2023-11-25 19:53:12','2023-11-25 19:53:12');
+INSERT INTO `movie` VALUES (1,'Titanic',1997,'drama','2023-11-07 19:54:30',NULL),(2,'Avatar',2009,'Sci-Fi',NULL,NULL),(4,'Aliens',1986,'Sci-Fi',NULL,NULL),(5,'Inception',2010,'Sci-Fi',NULL,NULL),(6,'Tenet',2010,'Sci-Fi','2023-11-07 20:54:15',NULL),(7,'Jhon Wick 4',2023,'Action','2023-11-12 20:15:57',NULL),(8,'Jhon Wick 3',2019,'Action','2023-11-12 20:15:57',NULL),(9,'Jhon Wick 2',2017,'Action','2023-11-12 20:15:57',NULL),(20,'Test movie for comment',2023,'Drama','2023-11-25 18:48:47','2023-11-25 18:48:47'),(22,'Quill',2000,'Drama','2023-11-25 19:53:12','2023-11-25 19:53:12'),(23,'Testing movie for transaction',NULL,NULL,'2023-12-03 18:41:01','2023-12-03 18:41:01'),(24,'Testing movie 2 for transaction',NULL,NULL,'2023-12-03 18:42:55','2023-12-03 18:42:55'),(27,'Testing movie 3 for transaction',NULL,NULL,'2023-12-03 18:46:36','2023-12-03 18:46:36');
 /*!40000 ALTER TABLE `movie` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -140,7 +165,7 @@ CREATE TABLE `movie_details` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -149,7 +174,7 @@ CREATE TABLE `movie_details` (
 
 LOCK TABLES `movie_details` WRITE;
 /*!40000 ALTER TABLE `movie_details` DISABLE KEYS */;
-INSERT INTO `movie_details` VALUES (1,1,'Titanic is Good movie',NULL,NULL),(2,2,'Avatar is good sci-fi movie',NULL,NULL),(3,4,'Aliens is good',NULL,NULL),(7,15,'Test movie 6 details updated',NULL,'2023-11-21 20:08:02');
+INSERT INTO `movie_details` VALUES (1,1,'Titanic is Good movie',NULL,NULL),(2,2,'Avatar is good sci-fi movie',NULL,NULL),(3,4,'Aliens is good',NULL,NULL),(7,15,'Test movie 6 details updated',NULL,'2023-11-21 20:08:02'),(9,23,'Movie Testing 4 Details','2023-12-03 18:41:01','2023-12-03 18:41:01'),(10,24,'Movie Testing 2 Details','2023-12-03 18:42:55','2023-12-03 18:42:55'),(13,27,'Movie Testing 3 Details','2023-12-03 18:46:36','2023-12-03 18:46:36');
 /*!40000 ALTER TABLE `movie_details` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -162,4 +187,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-25 20:24:25
+-- Dump completed on 2023-12-03 20:20:49
